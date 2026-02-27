df13aa4bad64cf5f85ebaf3d851f7557d72196e9
noimzip <gudoumaki@proton.me>
2026-02-27 23:20:57 +0900

    Feat: Recording button click counts and adding scores upon button clicks changed to variables

--- src/game.js
+++ src/game.js
@@ -1,14 +1,18 @@
 let lifetime_score; //game-lifetime-score
 let score = 0; //game-score
-let clicked_times;
+let cps = 1; //game-click-per-score
+let overall_clicked_times = 0; //game-button-clicked-times
 let lifetime_playtime; //game-lifetime-playtime
 let playtime; //game-playtime
 
 export function ButtonClick(element) {
   const counter = document.getElementById("counter");
+  const overall_main_button_clicked_times = document.getElementById("overall-main-button-clicked-times");
   const Count = () => {
-    score += 1;
+    score += cps;
+    overall_clicked_times += 1;
     counter.innerHTML = `Count is ${score}`;
+    overall_main_button_clicked_times.innerHTML = `Overall button click times: ${overall_clicked_times}`;
     //console.count("score");
     //console.log(isMultipleOfTen(score));
   }
@@ -16,11 +20,17 @@ export function ButtonClick(element) {
 }
 
 window.onload = function() {
+  const overall_main_button_clicked_times = document.getElementById("overall-main-button-clicked-times");
   const storage_score = parseInt(localStorage.getItem('score')) || 0;
+  const storage_cps = parseInt(localStorage.getItem('cps')) || 1;
+  const storage_overall_clicked_times = parseInt(localStorage.getItem('overall_clicked_times')) || 0;
   const storage_autosave = JSON.parse(localStorage.getItem('autosave'));
   score = storage_score;
+  cps = storage_cps;
+  overall_clicked_times = storage_overall_clicked_times;
   autosave.checked = storage_autosave;
   counter.innerHTML = `Count is ${score}`;
+  overall_main_button_clicked_times.innerHTML = `Overall button click times: ${overall_clicked_times}`;
 }
 
 export function GameReset(element) {
@@ -30,6 +40,8 @@ export function GameReset(element) {
       const SecondFactorConfirmReset = confirm("本当に本当にリセットしますか?");
       if (SecondFactorConfirmReset) {
         localStorage.removeItem('score');
+        localStorage.removeItem('cps');
+        localStorage.removeItem('overall_clicked_times');
         localStorage.removeItem('autosave');
         location.reload();
       }
@@ -42,21 +54,29 @@ export function GameReset(element) {
 //}
 
 export function Save(element) {
+  const saved_dialog = document.getElementById("saved-dialog");
   const TriggerSave = () => {
     localStorage.setItem('score', score);
+    localStorage.setItem('cps', cps);
+    localStorage.setItem('overall_clicked_times', overall_clicked_times);
     localStorage.setItem('autosave', autosave.checked);
-    console.log("Saved.");
+    saved_dialog.style.display = "block";
+    //console.log("Saved.");
   }
   element.addEventListener('click', TriggerSave);
+
+  const HideSavedDialog = () => {
+    saved_dialog.style.display = "none";
+  }
 
   const autosave = document.getElementById("autosave");
   let TriggerAutoSave;
   const AutoSaveConditionCheck = () => {
     if (autosave.checked) {
-      console.log("autosave_on");
-      TriggerAutoSave = setInterval(TriggerSave, 10000);
+      //console.log("autosave_on");
+      TriggerAutoSave = setInterval(TriggerSave, 60000);
     } else {
-      console.log("autosave_off");
+      //console.log("autosave_off");
       clearInterval(TriggerAutoSave);
     }
   }
--- src/main.js
+++ src/main.js
@@ -2,13 +2,23 @@ import './style.css'
 import { ButtonClick, GameReset, Save } from './game.js'
 
 document.querySelector('#app').innerHTML = `
+  <header>
+    <div id="saved-dialog">
+      saved.
+    </div>
+    <div id="upgrades-section">
+      <span class="section-title">Upgrades</span>
+      <div id="upgrades">
+      </div>
+    </div>
+  </header>
   <div>
     <div id="counter"></div>
     <button id="main-button" type="button"></button>
     <div id="buttons" class="right bottom">
+      <div id="overall-main-button-clicked-times"></div>
       <label>AutoSave:<input id="autosave" type="checkbox" checked></label>
       <button id="save-button" type="button">Save</button>
-      <button id="upgrade-button" type="button">Upgrades</button>
       <button id="status-button" type="button">Status</button>
     </div>
     <button id="reset-button" class="left bottom" type="button">Reset</button>
