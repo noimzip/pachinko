39bcc7e8a9dc520d044d86d0bfe576e7cd1d8bc2
noimzip <gudoumaki@proton.me>
2026-02-28 23:05:03 +0900

    Change: Enable button designs to be managed by class

--- src/main.js
+++ src/main.js
@@ -18,8 +18,10 @@ document.querySelector('#app').innerHTML = `
       <div id="overall-main-button-clicked-times"></div>
       <div id="overall-play-time"></div>
       <label>AutoSave:<input id="autosave-checkbox" type="checkbox" checked></label>
-      <button id="game-data-save-button" type="button">Save</button>
-      <button id="game-status-button" type="button">Status</button>
+      <button id="game-data-save-button" class="basic-button" type="button">Save</button>
+      <button id="game-status-button" class="basic-button" type="button">Status</button>
+      <button id="export-button" class="basic-button" type="button">Export</button>
+      <button id="import-button" class="basic-button" type="button">Import</button>
     </div>
     <button id="game-data-remove-button" class="left bottom" type="button">Reset</button>
   </div>
