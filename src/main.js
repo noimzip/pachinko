1d99f2f62a77e41f5b63561e65275af77bda2d0c
noimzip <gudoumaki@proton.me>
2026-03-15 00:12:18 +0900

    Refactor: Correcting overly elaborate styling

--- src/main.js
+++ src/main.js
@@ -2,29 +2,22 @@ import './style.css'
 import { gameDataExport, gameDataImport, unlockContent, createModal, MainButtonModel, MainButtonView, MainButtonController, DataManagementModel, DataManagementView, DataManagementController } from './game.js'
 
 document.querySelector('#app').innerHTML = `
-  <header>
-    <div id="notification-area">
-    </div>
-  </header>
-  <div>
-    <div id="header-stats-section">
-      <div id="score-counter"></div>
-      <div id="clicks-per-second"></div>
-    </div>
-    <button id="main-button" type="button"></button>
-    <div id="buttons" class="right bottom">
-      <label>AutoSave:<input id="autosave-checkbox" type="checkbox" checked></label>
-      <button id="game-data-save-button" class="basic-button" type="button">Save<div class="explain-sentence">(CTRL + S)</div></button>
-      <button id="game-status-button" class="basic-button" type="button">Status</button>
-      <button id="game-upgrades-button" class="basic-button" type="button" aria-disabled="true">Upgrades<div class="explain-sentence" id="upgrade-cost">(Unlock: 100)</div></button>
-      <button id="export-button" class="basic-button" type="button">Export<div class="explain-sentence">(CTRL + E)</div></button>
-      <label for="import-button" class="basic-button">
-        Import<div class="explain-sentence">(CTRL + I)</div>
-      </label>
-      <input type="file" id="import-button" style="display:none">
-    </div>
-    <button id="game-data-remove-button" class="basic-button left bottom" type="button">Reset</button>
+  <div id="notification-area"></div>
+  <div id="header-stats-section">
+    <div id="score-counter"></div>
+    <div id="clicks-per-second"></div>
   </div>
+  <button id="main-button" type="button"></button>
+  <div id="buttons" class="right bottom">
+    <label>AutoSave:<input id="autosave-checkbox" type="checkbox" checked></label>
+    <button id="game-data-save-button" type="button">Save<div class="explain-sentence">(CTRL + S)</div></button>
+    <button id="game-status-button" type="button">Status</button>
+    <button id="game-upgrades-button" type="button" aria-disabled="true">Upgrades<div class="explain-sentence" id="upgrade-cost">(Unlock: 100)</div></button>
+    <button id="export-button" type="button">Export<div class="explain-sentence">(CTRL + E)</div></button>
+    <label for="import-button">Import<div class="explain-sentence">(CTRL + I)</div></label>
+    <input type="file" id="import-button" style="display:none">
+  </div>
+  <button id="game-data-remove-button" class="left bottom" type="button">Reset</button>
 `
 const dataManagementController = new DataManagementController(new DataManagementModel(), new DataManagementView());
 window.addEventListener('DOMContentLoaded', dataManagementController.loadGame());
