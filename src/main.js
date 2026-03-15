b101beb2e30f717eb459637b90512f0f97354603
noimzip <gudoumaki@proton.me>
2026-03-15 10:09:27 +0900

    Fix: ID names that were not corrected in the previous commit

--- src/main.js
+++ src/main.js
@@ -18,19 +18,19 @@ document.querySelector('#app').innerHTML = `
   </div>
   <button id="game-data-reset-button" class="left bottom" type="button">Reset</button>
 `
-const dataManagementController = new DataManagementController(new DataManagementModel(), new DataManagementView());
+const dataManagementController = new DataManagementController(new DataManagementModel(), new DataManagementView(), "autosave-trigger-checkbox");
 window.addEventListener('DOMContentLoaded', dataManagementController.loadGame());
 dataManagementController.saveGame(document.getElementById("game-data-save-button"));
-dataManagementController.resetGame(document.getElementById("game-data-remove-button"));
-dataManagementController.autoSaveGame(document.getElementById("autosave-checkbox"));
+dataManagementController.resetGame(document.getElementById("game-data-reset-button"));
+dataManagementController.autoSaveGame(document.getElementById("autosave-trigger-checkbox"));
 
 const mainButtonController = new MainButtonController(new MainButtonModel(), new MainButtonView());
 mainButtonController.init(document.getElementById("main-button"));
 
-gameDataExport(document.getElementById("export-button"));
-gameDataImport(document.getElementById("import-button"));
-unlockContent(document.getElementById("game-upgrades-button"), document.getElementById("upgrade-cost"), 100);
-createModal(document.getElementById("game-status-button"), `
+gameDataExport(document.getElementById("game-data-export-trigger-button"));
+gameDataImport(document.getElementById("game-data-import-trigger-button"));
+unlockContent(document.getElementById("upgrade-modal-trigger-button"), 100);
+createModal(document.getElementById("status-modal-trigger-button"), `
     <div id="status-section">
       <span class="section-title">Status</span>
       <div id="status-area">
@@ -40,7 +40,7 @@ createModal(document.getElementById("game-status-button"), `
       </div>
     </div>
   `);
-createModal(document.getElementById("game-upgrades-button"), `
+createModal(document.getElementById("upgrade-modal-trigger-button"), `
     <div id="upgrades-section">
       <span class="section-title">Upgrades</span>
       <div id="upgrades-area">
